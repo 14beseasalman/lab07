5017ad94fcf70a9f8986f9d6e107f485b1dff495
Asad Salman <me@asad.co>
2017-04-19 13:49:36 +0500

    first commit

---
+++ src/lab0707/Challenge.java
@@ -0,0 +1,9 @@
+package lab0707;
+
+public class Challenge {
+	Integer challenge_id;
+	Integer challenge_no;
+	Float time_take;
+	Boolean state;
+	Integer round_id;
+}
---
+++ src/lab0707/Round.java
@@ -0,0 +1,10 @@
+package lab0707;
+
+public class Round {
+	Integer round_id;
+	String user_id;
+	String scheme;
+	Float time_per_input;
+	Boolean state;
+	
+}
